70919d0938fb2b03f8908832610ae95d0ecdb0c2
田中信秋 <sinshu.d@gmail.com>
2022-05-15 12:07:31 +0900

    Add readme.

---
+++ Workspace/Examples.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using MeltySynth;
+using DrippyAL;
+
+public static class Examples
+{
+    public static void OneSecSine()
+    {
+        var sampleRate = 44100;
+        var frequency = 440;
+        var data = Enumerable
+            .Range(0, sampleRate)
+            .Select(t => (short)(32000 * MathF.Sin(2 * MathF.PI * frequency * t / sampleRate)))
+            .ToArray();
+
+        using (var device = new AudioDevice())
+        using (var wave = new WaveData(device, sampleRate, 1, data))
+        using (var channel = new Channel(device))
+        {
+            channel.Play(wave);
+
+            Console.ReadKey();
+        }
+    }
+
+    public static void StreamingSine()
+    {
+        var sampleRate = 44100;
+        var frequency = 440;
+
+        using (var device = new AudioDevice())
+        using (var stream = new AudioStream(device, sampleRate, 1))
+        {
+            var phase = 0F;
+            var delta = 2 * MathF.PI * frequency / sampleRate;
+
+            stream.Play(block =>
+            {
+                for (var t = 0; t < block.Length; t++)
+                {
+                    block[t] = (short)(32000 * MathF.Sin(phase));
+                    phase += delta;
+                    if (phase > 2 * MathF.PI)
+                    {
+                        phase -= 2 * MathF.PI;
+                    }
+                }
+            });
+
+            Console.ReadKey();
+        }
+    }
+
+    public static void MidiSynthesis()
+    {
+        var sampleRate = 44100;
+        var synthesizer = new Synthesizer("TimGM6mb.sf2", sampleRate);
+        var sequencer = new MidiFileSequencer(synthesizer);
+        var midiFile = new MidiFile(@"C:\Windows\Media\flourish.mid");
+        sequencer.Play(midiFile, true);
+
+        using (var device = new AudioDevice())
+        using (var stream = new AudioStream(device, sampleRate, 2))
+        {
+            stream.Play(data => sequencer.RenderInterleavedInt16(data));
+
+            Console.ReadKey();
+        }
+    }
+}
--- Workspace/Program.cs
+++ Workspace/Program.cs
@@ -6,23 +6,5 @@ public static class Program
 {
     public static void Main()
     {
-        var sampleRate = 44100;
-
-        var synthesizer = new Synthesizer("TimGM6mb.sf2", sampleRate);
-        var sequencer = new MidiFileSequencer(synthesizer);
-        var midiFile = new MidiFile(@"C:\Windows\Media\flourish.mid");
-        sequencer.Play(midiFile, true);
-
-        using (var device = new AudioDevice())
-        using (var stream = new AudioStream(device, sampleRate, 2))
-        {
-            stream.Play(data => sequencer.RenderInterleavedInt16(data));
-
-            Console.ReadKey();
-
-            stream.Pitch = 0.5F;
-
-            Console.ReadKey();
-        }
     }
 }
